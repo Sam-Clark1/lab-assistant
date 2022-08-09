3eecdad09100acd3e2de7c558a151c7d98c33ebb
Sam Clark <samclark2399@gmail.com>
2022-08-08 22:47:11 -0400

    added functions for interacting with backend api

---
+++ src/api/auth.service.js
@@ -0,0 +1,40 @@
+import axios from "./axios";
+const register = (username, email, password) => {
+    return axios.post("signup", {
+      username,
+      email,
+      password,
+    });
+  };
+  
+  const login = (username, password) => {
+    return axios
+      .post("signin", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+  
+        return response.data;
+      });
+  };
+  
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+  
+  const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+  
+  const AuthService = {
+    register,
+    login,
+    logout,
+    getCurrentUser,
+  };
+  
+  export default AuthService;
