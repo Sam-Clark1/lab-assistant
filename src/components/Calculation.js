47b9be8ac8023542e676d989a07f1daca3945b77
Sam Clark <samclark2399@gmail.com>
2022-08-15 12:50:49 -0400

    changed file name

--- src/components/Calculation.js
+++ src/components/Calculation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import RVACalc from "./calculations/RVACalc";
+import RvaCalc from "./calculations/RvaCalc";
 import GramsCalc from "./calculations/GramsCalc";
 import LitersCalc from "./calculations/LitersCalc";
 import EnzymeAmount from "./calculations/EnzymeAmount";
@@ -11,7 +11,7 @@ export default function Calculations(props) {
     const params = useParams();
     const {calc} = params;
     if (calc === 'RVA Endpoint Viscosity') {
-        return <RVACalc userFavorites={userFavorites} setUserFavorites={setUserFavorites}/>
+        return <RvaCalc userFavorites={userFavorites} setUserFavorites={setUserFavorites}/>
     } else if (calc === 'Grams') {
         return <GramsCalc userFavorites={userFavorites} setUserFavorites={setUserFavorites} />
     } else if (calc === 'Liters') {
